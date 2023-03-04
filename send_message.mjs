6433222787e0b5005cfb9d64f396437ddbdc80db
zhuweiyou <i@zhuweiyou.com>
2023-03-04 09:42:20 +0800

    更新

--- send_message.mjs
+++ send_message.mjs
@@ -1,14 +1,22 @@
 import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
 
 export async function send_message(req, res) {
-    const { access_token, reverse_proxy, prompt, prompt_prefix, prompt_suffix, conversation_id, parent_message_id } =
-        req.body
+    const {
+        access_token,
+        reverse_proxy,
+        prompt,
+        prompt_prefix,
+        prompt_suffix,
+        conversation_id,
+        parent_message_id,
+    } = req.body
     if (!access_token || !prompt) {
         throw new Error('invalid [access_token] or [prompt]')
     }
     const chatgpt_api = new ChatGPTUnofficialProxyAPI({
         accessToken: access_token,
-        apiReverseProxyUrl: reverse_proxy ?? 'https://chat.duti.tech/api/conversation',
+        apiReverseProxyUrl:
+            reverse_proxy ?? 'https://chat.duti.tech/api/conversation',
     })
     const send_message_response = await chatgpt_api.sendMessage(prompt, {
         conversationId: conversation_id,
