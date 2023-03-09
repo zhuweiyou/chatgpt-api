d2dd4cdaca98dcb3739d4c7cfb3a19b012837c82
zhuweiyou <i@zhuweiyou.com>
2023-03-10 00:24:18 +0800

    format

--- get_access_token.mjs
+++ get_access_token.mjs
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
 export async function get_access_token(req, res) {
-    const { email, password } = req.body
-    if (!email || !password) {
-        throw new Error('invalid [email] or [password]')
-    }
-    // 如果需要自建auth服务, 请fork: https://github.com/zhuweiyou/chatgpt-auth
-    const { data } = await axios({
-        method: 'POST',
-        url: 'https://chatgpt-auth.vercel.app/',
-        headers: {
-            'X-Email': email,
-            'X-Password': password,
-        },
-    })
-    if (!data) {
-        throw new Error('failed to get [access_token]')
-    }
-    res.json({ access_token: data })
+	const { email, password } = req.body
+	if (!email || !password) {
+		throw new Error('invalid [email] or [password]')
+	}
+	// 如果需要自建auth服务, 请fork: https://github.com/zhuweiyou/chatgpt-auth
+	const { data } = await axios({
+		method: 'POST',
+		url: 'https://chatgpt-auth.vercel.app/',
+		headers: {
+			'X-Email': email,
+			'X-Password': password,
+		},
+	})
+	if (!data) {
+		throw new Error('failed to get [access_token]')
+	}
+	res.json({ access_token: data })
 }
